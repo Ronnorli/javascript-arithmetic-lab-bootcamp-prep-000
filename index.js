6d65b2f93e089d53782ace2792d0c7ce8178d715
Ronald Li <ronaldbrooklynli@gmail.com>
2017-11-29 22:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,12 +23,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt(n);
+  return parseInt(n, 10);
 }
 
-function makeInt(n) {
-  n = 10;
-}
 
 function makeInt(n) {
   return parseInt('nonesense!');
